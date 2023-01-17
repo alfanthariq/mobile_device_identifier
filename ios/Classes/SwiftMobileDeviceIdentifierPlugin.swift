0c302ed95882d05103a63807a3d75d943237bbd0
Nafi' Alfanthariq <naffripperz@gmail.com>
2023-01-17 16:07:02 +0700

    add ios support

--- ios/Classes/SwiftMobileDeviceIdentifierPlugin.swift
+++ ios/Classes/SwiftMobileDeviceIdentifierPlugin.swift
@@ -1,14 +1,42 @@
 import Flutter
 import UIKit
+import JNKeychain
 
 public class SwiftMobileDeviceIdentifierPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "mobile_device_identifier", binaryMessenger: registrar.messenger())
-    let instance = SwiftMobileDeviceIdentifierPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+      let channel = FlutterMethodChannel(name: "mobile_device_identifier", binaryMessenger: registrar.messenger())
+      let instance = SwiftMobileDeviceIdentifierPlugin()
+      registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "getDeviceId":
+        let key = "com.alfanthariq.mobile_device_identifier/device_id"
+        result(self.getDeviceIdentifierFromKeychain(key: key))
+    default:
+        result(FlutterMethodNotImplemented)
+        fatalError()
+    }
+  }
+
+  func getDeviceIdentifierFromKeychain(key : String) -> String {
+    // try to get value from keychain
+    var deviceUDID = self.keychain_valueForKey(key) as? String
+    if deviceUDID == nil {
+        deviceUDID = UIDevice.current.identifierForVendor!.uuidString
+        // save new value in keychain
+        self.keychain_setObject(deviceUDID! as AnyObject, forKey: key)
+    }
+    return deviceUDID!
+  }
+
+  func keychain_setObject(_ object: AnyObject, forKey: String) {
+    JNKeychain.saveValue(object, forKey: forKey)
+  }
+
+  func keychain_valueForKey(_ key: String) -> AnyObject? {
+    let value = JNKeychain.loadValue(forKey: key)
+    return value as AnyObject?
   }
 }
--- ios/mobile_device_identifier.podspec
+++ ios/mobile_device_identifier.podspec
@@ -9,13 +9,14 @@ Pod::Spec.new do |s|
   s.description      = <<-DESC
 A new Flutter plugin project.
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'http://alfanthariq.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Nafi Alfanthariq' => 'mnalfanthariq@gmail.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+  s.dependency 'JNKeychain'
+  s.platform = :ios, '11.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
